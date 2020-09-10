d9ec0248554e14bee1adfd999d646076636d722b
Amy Zala <fallen.avatar28@gmail.com>
2020-09-10 01:43:50 -0400

    adding some files i missed

---
+++ WebHosts/WebHost.OperatorApi/Controllers/CapabilityController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using WebHost.OperatorApi.Capability;
+
+namespace WebHost.OperatorApi.Controllers {
+	[ApiController]
+	public class CapabilityController : ControllerBase {
+		private readonly ICapabilityRepository _capabilityRepository;
+		private readonly ILogger<CapabilityController> _logger;
+
+		public CapabilityController( ILogger<CapabilityController> logger,
+									ICapabilityRepository capabilityRepository ) {
+			_logger = logger;
+			_capabilityRepository = capabilityRepository;
+		}
+
+		[HttpGet]
+		[Route("check")]
+		public async Task<HostInformation> CheckAsync( string environment, int build ) {
+			return await _capabilityRepository.GetHostInformationAsync(environment, build);
+		}
+
+		[HttpGet]
+		[Route("api/v1/products/{productName}")]
+		public async Task<ProductInformation> Products( string productName ) {
+			return await _capabilityRepository.GetProductInformationAsync(productName);
+		}
+	}
+}
---
+++ WebHosts/WebHost.OperatorApi/WebServer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebHost.OperatorApi {
+	public class WebServer : Shared.Web.BaseWebServer {
+		public WebServer( IConfiguration configuration ) : base(configuration) { }
+
+		protected override void ConfigureChildServices( IServiceCollection services ) { }
+		protected override void ConfigureChild( IApplicationBuilder app, IWebHostEnvironment env ) { }
+	}
+}
