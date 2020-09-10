6de857fc387b13a77c259a937ea830162dcddefd
Amy Zala <fallen.avatar28@gmail.com>
2020-09-10 02:08:55 -0400

    more missing files

---
+++ WebHosts/WebHost.OperatorApi/Capability/ICapabilityRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace WebHost.OperatorApi.Capability {
+	public interface ICapabilityRepository {
+		Task<HostInformation> GetHostInformationAsync( string environment, int build );
+		Task<ProductInformation> GetProductInformationAsync( string productName );
+	}
+}
