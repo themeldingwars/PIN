7445a89ffa5fac218cedbfd71c52cfa2051a7cd7
Amy Zala <fallen.avatar28@gmail.com>
2020-09-18 13:57:47 -0400

    Trying to get in game Adding 3 other controllers

---
+++ UdpHosts/MyGameServer/Packets/GSS/Character/LocalEffectsController/KeyFrame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Shared.Udp;
+
+namespace MyGameServer.Packets.GSS.Character.LocalEffectsController {
+	[GSSMessage( Enums.GSS.Controllers.Character_LocalEffectsController, (byte)Enums.GSS.Character.Events.KeyFrame )]
+	public class KeyFrame {
+		[Field]
+		public ulong PlayerID;
+		[Field]
+		[Length(8)]
+		public IList<byte> UnkBytes;
+
+		public KeyFrame() {
+			UnkBytes = new List<byte> {
+                0xff, 0xff, 0xff, 0xff,
+                0xff, 0xff, 0xff, 0xff,
+			};
+		}
+	}
+}
---
+++ UdpHosts/MyGameServer/Packets/GSS/Character/MissionAndMarkerController/KeyFrame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Shared.Udp;
+
+namespace MyGameServer.Packets.GSS.Character.MissionAndMarkerController {
+	[GSSMessage( Enums.GSS.Controllers.Character_MissionAndMarkerController, (byte)Enums.GSS.Character.Events.KeyFrame )]
+	public class KeyFrame {
+		[Field]
+		public ulong PlayerID;
+		[Field]
+		[Length(12)]
+		public IList<byte> UnkBytes;
+
+		public KeyFrame() {
+			UnkBytes = new List<byte> {
+                0xff, 0xff, 0xff, 0xff,
+                0xff, 0xff, 0xff, 0xff,
+				0xff, 0xff, 0xff, 0xff,
+			};
+		}
+	}
+}
