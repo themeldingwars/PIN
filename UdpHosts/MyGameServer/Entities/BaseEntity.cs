eec23d97a947ce55842d9dae52afb934db994507
Amy Zala <fallen.avatar28@gmail.com>
2020-09-22 14:06:09 -0400

    rebase

---
+++ UdpHosts/MyGameServer/Entities/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGameServer.Entities {
+	public class BaseEntity : IEntity {
+		public ulong EntityID { get; }
+		public IShard Owner { get; }
+		public IDictionary<Enums.GSS.Controllers, ushort> ControllerRefMap { get; }
+
+		public BaseEntity( IShard owner, ulong id ) {
+			Owner = owner;
+			EntityID = id;
+			ControllerRefMap = new ConcurrentDictionary<Enums.GSS.Controllers, ushort>();
+		}
+
+		public void RegisterController(Enums.GSS.Controllers controller) {
+			ControllerRefMap.Add(controller, Owner.AssignNewRefId(this, controller));
+		}
+	}
+}
