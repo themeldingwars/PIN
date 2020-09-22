da0be30e25b6376adac241dcb6ca9a8ea4de266c
Amy Zala <fallen.avatar28@gmail.com>
2020-09-22 08:07:51 -0400

    Back to where I was, but with cleaned up code and removed auto bigendian flipping

--- UdpHosts/MyGameServer/Packets/Matrix/SynchronizationRequest.cs
+++ UdpHosts/MyGameServer/Packets/Matrix/SynchronizationRequest.cs
@@ -7,28 +7,9 @@ using Shared.Udp;
 
 namespace MyGameServer.Packets.Matrix {
 	[MatrixMessage(Enums.MatrixPacketType.SynchronizationRequest)]
-	[StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct SynchronizationRequest : IWritable {
-		public static SynchronizationRequest Parse( Packet packet ) {
-			var ret = new SynchronizationRequest();
-
-			ret.Unk = packet.Read(packet.BytesRemaining).ToArray();
-
-			return ret;
-		}
-
-		public byte[] Unk;
-
-		public Memory<byte> Write() {
-			Memory<byte> mem = new byte[Unk.Length];
-
-			Unk.CopyTo(mem);
-
-			return mem;
-		}
-
-		public Memory<byte> WriteBE() {
-			return Write();
-		}
+    public class SynchronizationRequest {
+		[Field]
+		[Length(1)]
+		public IList<byte> Unk;
 	}
 }
--- UdpHosts/MyGameServer/Packets/Matrix/WelcomeToTheMatrix.cs
+++ UdpHosts/MyGameServer/Packets/Matrix/WelcomeToTheMatrix.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
 
+using Shared.Udp;
+
 namespace MyGameServer.Packets.Matrix {
 	[MatrixMessage(Enums.MatrixPacketType.WelcomeToTheMatrix)]
-	[StructLayout(LayoutKind.Sequential, Pack = 1)]
-	public readonly struct WelcomeToTheMatrix {
-		public readonly ulong InstanceID;
-		public readonly ushort Unk1;
-		public readonly ushort Unk2;
-
-		public WelcomeToTheMatrix(ulong instanceID, ushort unk1, ushort unk2) {
-			InstanceID = instanceID;
-			Unk1 = unk1;
-			Unk2 = unk2;
-		}
+	public class WelcomeToTheMatrix {
+		[Field]
+		public ulong InstanceID;
+		[Field]
+		public ushort Unk1;
+		[Field]
+		public ushort Unk2;
 	}
 }
