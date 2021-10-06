258d9982beabd5078f55cfc76ee4c7ebe7ad6bda
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2021-10-06 09:39:52 +0200

    Add shared assembly version

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("The Melding Wars")]
+[assembly: AssemblyCopyright("Copyright ©2019-2021 by The Melding Wars")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
