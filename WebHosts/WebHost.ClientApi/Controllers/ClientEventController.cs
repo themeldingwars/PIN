9c650bb9466d2d155045a1914ebc7c5003161f84
Leon Scharfenort <l.dapo@gmx.net>
2022-10-30 04:10:00 +0100

    Add ClientEventController with corresponding models

---
+++ WebHosts/WebHost.ClientApi/Controllers/ClientEventController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebHost.ClientApi.Models.ClientEvent;
+
+namespace WebHost.ClientApi.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class ClientEventController : ControllerBase
+    {
+        [Route("client_event")]
+        [HttpPost]
+        public object Post([FromBody]ClientEvent payload)
+        {
+            Log.Logger.Information(payload.ToString());
+            return new { };
+        }
+    }
+}
