73cebdc8b6550b1b39f4dac43e2ede7195def369
Leon Scharfenort <l.dapo@gmx.net>
2022-10-30 07:04:46 +0100

    Add configuration for GameServer

---
+++ UdpHosts/GameServer/GameServerSettings.cs
@@ -0,0 +1,20 @@
+﻿using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    /// <summary>
+    /// Holds the settings for the server
+    /// </summary>
+    public class GameServerSettings
+    {
+        /// <summary>
+        /// The log level to use for the logger. Any messages below this level won't be printed to console.
+        /// </summary>
+        public LogEventLevel? LogLevel { get; set; }
+    }
+}
--- UdpHosts/GameServer/Program.cs
+++ UdpHosts/GameServer/Program.cs
@@ -1,30 +1,92 @@
-﻿using Serilog;
+﻿using CommandLine;
+using Newtonsoft.Json;
+using Serilog;
+using Serilog.Events;
 using Shared.Common;
 using Shared.Udp;
 using System;
 using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.IO;
 
 namespace GameServer
 {
     internal static class Program
     {
         public static ILogger Logger { get; private set; }
+        public static GameServerSettings GameServerSettings { get; private set; }
 
         private static void Main(string[] arguments)
         {
+
+            LoadSettings();
+            ParseAndApplyCliOptions(arguments);
+
             Logger = new LoggerConfiguration()
-                     .MinimumLevel.Verbose()
+                     .MinimumLevel.Is(GameServerSettings.LogLevel.Value!)
                      .WriteTo.Console(theme: SerilogTheme.Custom)
                      .CreateLogger();
 
+
+            // TODO: add DI?
+
             PacketServer.Logger = Logger;
 
-            // TODO: Handle/allow args and configuration, add DI?
+            
             Span<byte> ranSpan = stackalloc byte[8];
             new Random().NextBytes(ranSpan.Slice(2, 6));
             var serverId = BinaryPrimitives.ReadUInt64LittleEndian(ranSpan);
             var server = new GameServer(25001, serverId);
             server.Run();
+        }
+
+        /// <summary>
+        /// Load the settings from the current directory.
+        /// "settings.development.json" takes precedence over "settings.json"
+        /// <remarks>The ConfigurationExtensions used in the WebHostManager are only available for the Windows platform, hence we need to implement it manually</remarks>
+        /// </summary>
+        private static void LoadSettings()
+        {
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "config");
+            var developmentSettingsPath = Path.Combine(basePath, "settings.development.json");
+            var settingsPath = Path.Combine(basePath, "settings.json");
+
+            var sourcePath = File.Exists(developmentSettingsPath) ? developmentSettingsPath : settingsPath;
+
+            try
+            {
+                var json = File.ReadAllText(sourcePath);
+                GameServerSettings = JsonConvert.DeserializeObject<GameServerSettings>(json);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Error when trying to read settings from \"{sourcePath}\" : {e}{Environment.NewLine}Using builtin default settings");
+                GameServerSettings = new GameServerSettings { LogLevel = LogEventLevel.Verbose };
+            }
+
+        }
+
+        private static void ParseAndApplyCliOptions(IEnumerable<string> arguments)
+        {
+            Parser.Default.ParseArguments<CliOptions>(arguments)
+                  .WithParsed(RunOptions)
+                  .WithNotParsed(HandleParseError);
+        }
+
+        private static void RunOptions(CliOptions options)
+        {
+            if (options.LogLevel != null)
+            {
+                GameServerSettings.LogLevel = options.LogLevel;
+            }
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errors)
+        {
+            foreach (var error in errors)
+            {
+                Console.Error.WriteLine("Error when parsing options: " + error);
+            }
         }
     }
 }
