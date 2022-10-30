e0231377c97fe6013d024bf33832300e2e355f1e
Leon Scharfenort <l.dapo@gmx.net>
2022-10-30 07:04:46 +0100

    Add configuration for GameServer

---
+++ UdpHosts/GameServer/CliOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    /// <summary>
+    /// Declares the options which are supported on the CLI
+    /// </summary>
+    public class CliOptions
+    {
+        [Option('l', "logLevel", Default = null, Required = false, HelpText = "The log level. Any messages below this level will not be printed to the console")]
+        public LogEventLevel? LogLevel { get; set; }
+    }
+}
