6ab032848104d644271ab3fe7d4ef4479bfbaf5c
Leon Scharfenort <l.dapo@gmx.net>
2022-10-30 04:10:00 +0100

    Add ClientEventController with corresponding models

---
+++ WebHosts/WebHost.ClientApi/Models/ClientEvent/ClientEventData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebHost.ClientApi.Models.ClientEvent
+{
+    /// <summary>
+    /// Transmitted to the API within a <see cref="ClientEvent"/>
+    /// </summary>
+    public class ClientEventData
+    {
+        /// <summary>
+        /// (unknown so far, presumably more details about the event)
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// The source of the event within the client (eg. "login")
+        /// </summary>
+        public string Source { get; set; }
+
+        /// <summary>
+        /// The the actual data in form of a JSON string
+        /// </summary>
+        public string Data { get; set; }
+    }
+}
