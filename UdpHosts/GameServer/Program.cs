2537766120b697a98c264896138fce8d042d8105
Leon Scharfenort <l.dapo@gmx.net>
2022-11-03 16:11:48 +0100

    Add missing Item entry for App.config in project file Change Program.cs structure to be more expressive Add code documentation

--- UdpHosts/GameServer/Program.cs
+++ UdpHosts/GameServer/Program.cs
@@ -18,9 +18,22 @@ namespace GameServer
 
         private static void Main(string[] arguments)
         {
-
+            // TODO: add DI?
+            LoadAppSettings();
             ParseAndApplyCliOptions(arguments);
+            SetupLogger();
 
+
+            var serverId = GenerateServerId();
+            var server = new GameServer(25001, serverId);
+            server.Run();
+        }
+
+        /// <summary>
+        /// Set the logger up
+        /// </summary>
+        private static void SetupLogger()
+        {
             var loggerConfig = new LoggerConfiguration()
                                .ReadFrom.AppSettings()
                                .WriteTo.Console(theme: SerilogTheme.Custom);
@@ -33,19 +46,35 @@ namespace GameServer
             Logger = loggerConfig
                 .CreateLogger();
 
-
-            // TODO: add DI?
-
             PacketServer.Logger = Logger;
-
-            
-            Span<byte> ranSpan = stackalloc byte[8];
-            new Random().NextBytes(ranSpan.Slice(2, 6));
-            var serverId = BinaryPrimitives.ReadUInt64LittleEndian(ranSpan);
-            var server = new GameServer(25001, serverId);
-            server.Run();
         }
 
+        /// <summary>
+        /// Load the settings from App.config
+        /// </summary>
+        private static void LoadAppSettings()
+        {
+            GameServerSettings = new GameServerSettings();
+            // no settings as of yet, expand this function should the need arise
+        }
+
+        /// <summary>
+        /// Generate the Server Id
+        /// ToDo: Incorporate the Sql Node Number as per https://gist.github.com/SilentCLD/881839a9f45578f1618db012fc789a71
+        /// </summary>
+        /// <returns></returns>
+        private static ulong GenerateServerId()
+        {
+            Span<byte> ranSpan = stackalloc byte[8];
+            new Random().NextBytes(ranSpan.Slice(2, 6));
+            var ret = BinaryPrimitives.ReadUInt64LittleEndian(ranSpan);
+            return ret;
+        }
+
+        /// <summary>
+        /// Parse the options passed via the command line and overwrite settings from the config
+        /// </summary>
+        /// <param name="arguments"></param>
         private static void ParseAndApplyCliOptions(IEnumerable<string> arguments)
         {
             Parser.Default.ParseArguments<CliOptions>(arguments)
@@ -53,6 +82,10 @@ namespace GameServer
                   .WithNotParsed(HandleParseError);
         }
 
+        /// <summary>
+        /// Handle the parsed options, essentially overwriting already present settings loaded from App.config
+        /// </summary>
+        /// <param name="options"></param>
         private static void RunOptions(CliOptions options)
         {
             if (options.LogLevel != null)
@@ -61,6 +94,10 @@ namespace GameServer
             }
         }
 
+        /// <summary>
+        /// If errors occur during the parsing of CLI options, they should be handled here
+        /// </summary>
+        /// <param name="errors"></param>
         private static void HandleParseError(IEnumerable<Error> errors)
         {
             foreach (var error in errors)
