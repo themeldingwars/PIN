a329a07040c3e8db7a289e662683c409243ed5a9
Leon Scharfenort <l.dapo@gmx.net>
2022-11-05 17:28:07 +0100

    Add untested SendIAero method to Channel Add IAero extensions, currently only providing a method to serialize a package to memory

---
+++ UdpHosts/GameServer/Extensions/IAero.cs
@@ -0,0 +1,20 @@
+﻿using Aero.Gen;
+using System;
+
+namespace GameServer.Extensions
+{
+    // ReSharper disable once InconsistentNaming
+    internal static class IAeroExtensions
+    {
+        /// <summary>
+        /// Serialize the instance to a region in memory
+        /// </summary>
+        /// <param name="aero"></param>
+        /// <param name="serializedData"></param>
+        internal static void SerializeToMemory(this IAero aero, out Memory<byte> serializedData)
+        {
+            serializedData = new Memory<byte>(new byte[aero.GetPackedSize()]);
+            aero.Pack(serializedData.Span);
+        }
+    }
+}
