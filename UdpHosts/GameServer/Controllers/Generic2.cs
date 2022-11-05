105743a6a43eee68934d1022cfb4d7671e5fd19c
Leon Scharfenort <l.dapo@gmx.net>
2022-11-05 08:28:46 +0100

    Move ScheduleUpdateRequest to the correct controller (as specified in AeroMessages) Add missing controller Methods to Generic2 controller as specified in AeroMessages

--- UdpHosts/GameServer/Controllers/Generic2.cs
+++ UdpHosts/GameServer/Controllers/Generic2.cs
@@ -1,5 +1,7 @@
 ﻿using GameServer.Enums.GSS.Generic;
 using GameServer.Packets;
+using GameServer.Packets.GSS.Generic;
+using System;
 
 namespace GameServer.Controllers
 {
@@ -10,8 +12,91 @@ namespace GameServer.Controllers
         {
         }
 
+        [MessageID((byte)Commands.UIToEncounterMessage)]
+        public void UiToEncounter(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.ServerProfiler_RequestNames)]
+        public void ServerProfiler_RequestNames(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.ScheduleUpdateRequest)]
+        public void ScheduleUpdateRequest(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+            var req = packet.Read<ScheduleUpdateRequest>();
+
+            player.LastRequestedUpdate = client.AssignedShard.CurrentTime;
+            player.RequestedClientTime = Math.Max(req.requestClientTime, player.RequestedClientTime);
+
+            if (!player.FirstUpdateRequested)
+            {
+                player.FirstUpdateRequested = true;
+                player.Respawn();
+            }
+
+            //Program.Logger.Error( "Update scheduled" );
+        }
+
+        [MessageID((byte)Commands.LocalProximityAbilitySuccess)]
+        public void LocalProximityAbilitySuccess(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.RemoteProximityAbilitySuccess)]
+        public void RemoteProximityAbilitySuccess(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.TrailRequest)]
+        public void TrailRequest(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.RequestLeaveZone)]
+        public void RequestLeaveZone(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
         [MessageID((byte)Commands.RequestLogout)]
         public void RequestLogout(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.RequestEncounterInfo)]
+        public void RequestEncounterInfo(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.RequestActiveEncounters)]
+        public void RequestActiveEncounters(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+
+        [MessageID((byte)Commands.VotekickRequest)]
+        public void VotekickRequest(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.VotekickResponse)]
+        public void VotekickResponse(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.GlobalCounterRequest)]
+        public void GlobalCounterRequest(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.CurrentLoadoutRequest)]
+        public void CurrentLoadoutRequest(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
+        {
+        }
+
+        [MessageID((byte)Commands.VendorProductRequest)]
+        public void VendorProductRequest(INetworkClient client, IPlayer player, ulong EntityID, GamePacket packet)
         {
         }
     }
