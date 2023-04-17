049c9b2990d4bcd4f3e7aeff2ee5bde9f6abb365
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2023-04-17 22:33:53 +0200

    Remove unnecessary nullable tags for string type

--- WebHosts/WebHost.ClientApi/Characters/ArmyController.cs
+++ WebHosts/WebHost.ClientApi/Characters/ArmyController.cs
@@ -345,18 +345,18 @@ public class Army
     public string Personality { get; set; }
     public string Motd { get; set; }
     public bool IsRecruiting { get; set; }
-    public string? CreatedAt { get; set; }
-    public string? UpdatedAt { get; set; }
+    public string CreatedAt { get; set; }
+    public string UpdatedAt { get; set; }
     public ulong? CommanderGuid { get; set; }
-    public string? TagPosition { get; set; }
+    public string TagPosition { get; set; }
     public uint? MinSize { get; set; }
     public uint? MaxSize { get; set;}
     public bool? Disbanded { get; set; }
-    public string? Website { get; set; }
+    public string Website { get; set; }
     public bool? MassEmail { get; set; }
-    public string? Region { get; set; }
-    public string? LoginMessage { get; set; }
-    public string? Timezone { get; set; }
+    public string Region { get; set; }
+    public string LoginMessage { get; set; }
+    public string Timezone { get; set; }
     public uint EstablishedAt { get; set; }
     public string Tag { get; set; }
     public string Language { get; set; }
@@ -388,8 +388,8 @@ public class ArmyMember
     public uint? CurrentLevel { get; set; }
     public uint? CurrentFrameSdbId { get; set; }
     public bool? IsOnline { get; set; }
-    public string? PublicNote { get; set; }
-    public string? OfficerNote { get; set; }
+    public string PublicNote { get; set; }
+    public string OfficerNote { get; set; }
     public string Name { get; set; }
 }
 
