aa775cd888128a4031951270f3c0ee17cff1c8dd
Sleepwalker <github@cloudswp.com>
2023-04-30 12:17:02 +0200

    Added bit shifts for falling and jetsprint

--- UdpHosts/GameServer/Entities/Character/CharMovementState.cs
+++ UdpHosts/GameServer/Entities/Character/CharMovementState.cs
@@ -5,11 +5,16 @@ namespace GameServer.Entities.Character;
 [Flags]
 public enum CharMovementState : short
 {
-    None,
+    JetSprint = -1 << 14,
+    None = 0,
     Crouch = 1 << 0,
     Unknown2 = 1 << 1, // This is only transmitted once by the client during connection, perhaps an "uninitialized" state?
     MovementKeys = 1 << 2,
     Sprint = 1 << 4,
+    /* Start Falling */
+    MovingTooQuickly1 = 1 << 10,
+    MovingTooQuickly2 = 1 << 11,
+    /* End Falling */
     TryingToMove = 1 << 12,
     IsMoving = 1 << 13,
     Unk = 1 << 14
