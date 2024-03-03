3c08f3b74c1537b69279b68ec12028bc72ff9e8b
Xsear <Xsear@users.noreply.github.com>
2024-03-03 05:03:20 +0100

    Add TargetInitiatorCommand

---
+++ UdpHosts/GameServer/Systems/Aptitude/Commands/Target/TargetInitiatorCommand.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using GameServer.Data.SDB.Records.apt;
+
+namespace GameServer.Aptitude;
+
+public class TargetInitiatorCommand : ICommand
+{
+    private TargetInitiatorCommandDef Params;
+
+    public TargetInitiatorCommand(TargetInitiatorCommandDef par)
+    {
+        Params = par;
+    }
+
+    public bool Execute(Context context)
+    {
+        // Console.WriteLine($"TargetSelfCommand Pre Target {context.Targets.FirstOrDefault()}");
+
+        // TODO: FormerTargets ?
+        context.Targets.Add(context.Initiator);
+
+        // Console.WriteLine($"TargetSelfCommand Post Target {context.Targets.FirstOrDefault()}");
+        return true;
+    }
+}
