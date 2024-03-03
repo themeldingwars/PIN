3507e0554161a879cfb7ec881c837c4fdabf9caa
Xsear <Xsear@users.noreply.github.com>
2024-03-03 03:56:52 +0100

    Expose position to apt

--- UdpHosts/GameServer/Systems/Aptitude/IAptitudeTarget.cs
+++ UdpHosts/GameServer/Systems/Aptitude/IAptitudeTarget.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using AeroMessages.Common;
 using AeroMessages.GSS.V66;
 
@@ -10,6 +11,7 @@ public interface IAptitudeTarget
     public ulong EntityId { get; } // From BaseEntity
     public EntityId AeroEntityId { get; } // From BaseEntity
     public IShard Shard { get; } // From BaseEntity
+    public Vector3 Position { get; } // From BaseEntity
 
     public List<EffectState> GetActiveEffects();
     public EffectState AddEffect(Effect effect, Context context);
