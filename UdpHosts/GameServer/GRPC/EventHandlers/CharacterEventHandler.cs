50c4411a958327c4c3e0f68621d745e4bf426c81
SzymonKaminski <SzymonKaminski@users.noreply.github.com>
2024-03-11 18:56:15 +0100

    Handle events from Grpc stream (#38)

---
+++ UdpHosts/GameServer/GRPC/EventHandlers/CharacterEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using GrpcGameServerAPIClient;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameServer.GRPC.EventHandlers;
+
+public static class CharacterEventHandler
+{
+    public static void HandleEvent(CharacterVisualsUpdated e, IDictionary<uint, INetworkPlayer> clients)
+    {
+        clients.Values.FirstOrDefault(p => p.CharacterId + 0xFE == e.CharacterGuid)
+               ?.CharacterEntity.LoadRemote(e.CharacterAndBattleframeVisuals);
+    }
+}
