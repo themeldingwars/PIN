f1e73fe6661b4f9fc4eb4b49412690f57fbb1f8f
Xsear <Xsear@users.noreply.github.com>
2024-03-29 14:52:21 +0100

    Admin combat flags

---
+++ UdpHosts/GameServer/Systems/Admin/Commands/CancelForcedMovementServerCommand.cs
@@ -0,0 +1,34 @@
+using AeroMessages.GSS.V66.Character.Event;
+
+namespace GameServer.Admin;
+
+[ServerCommand("Cancel ForcedMovement", "cancelfm <commandId>", "cancelfm")]
+public class CancelForcedMovementServerCommand : ServerCommand
+{
+    public override void Execute(string[] parameters, ServerCommandContext context)
+    {
+        if (context.SourcePlayer == null || context.SourcePlayer.CharacterEntity == null)
+        {
+            SourceFeedback("Cannot cancel forced movement without a valid player character", context);
+            return;
+        }
+
+        if (parameters.Length != 1)
+        {
+            SourceFeedback("Invalid arguments", context);
+            return;
+        }
+        
+        uint commandId = ParseUIntParameter(parameters[0]);
+        var character = context.SourcePlayer.CharacterEntity;
+        var player = character.Player;
+        var message = new ForcedMovementCancelled
+        {
+            CommandId = commandId,
+            ShortTime = context.Shard.CurrentShortTime,
+        };
+        player.NetChannels[ChannelType.ReliableGss].SendIAero(message, character.EntityId);
+
+        SourceFeedback($"Cancelling forced movement {commandId}", context);
+    }
+}
