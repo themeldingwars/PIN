9513db30fc5946472626e347960182a50642bf0c
Xsear <Xsear@users.noreply.github.com>
2024-09-26 21:27:36 +0200

    Mess with stat modifier id

--- UdpHosts/GameServer/Enums/StatModifierIdentifier.cs
+++ UdpHosts/GameServer/Enums/StatModifierIdentifier.cs
@@ -7,39 +7,34 @@ public enum StatModifierIdentifier : ushort
 {
     // TODO: Maybe rewrite this, not sure if the mapping is supposed to be 1 to 1
     RunSpeedMult = 1,
+    FwdRunSpeedMult = 2, // 1189
+    JumpHeightMult = 3, // 1189
+    DamageTaken = 4, // Damage Resistance? Boomerang, Heavy Armor
+    DamageDealt = 5, // 1189
+    Health = 6, // 1189
+    MaxHealth = 7, // 1189
+    Shields = 8, // 1189
+    MaxShields = 9, // 1189
+    AirControlMult = 10, // Air Control? Based on Hover Mode, command 859100 is loading Hover Mode Air Control stat right before 859099 which modifies this stat
+    Energy = 11,
+    MaxEnergy = 12,
+    EnergyRechargeDelay = 13,
+    EnergyRechargeRate = 14,
+    ThrustStrengthMult = 15,
+    ThrustAirControl = 16, // Air Control? Based Engineer Overclock
+    Friction = 17,
+    SinBonus = 18,
+    SinVulnerability = 19,
+    MaxTurnRate = 20, // Seems confirmed by msg 551119, command 1120999, effect 11686
     FireRateModifier = 21,
     AmmoConsumption = 22, // Based on Overcharge command 1626508, seems to hold up
+    AccuracyModifier = 23,
+    CooldownModifier = 24,
+    Progress = 25,
 
-    // Related
-    MaxTurnRate = 20, // Seems confirmed by msg 551119, command 1120999, effect 11686
-    GravityMult = 50, // Either GravityMult or TurnSpeed
-    TurnSpeed = 27, // Either GravityMult or TurnSpeed
-
-    Unknown_10 = 16, // Air Control? Based Engineer Overclock
-    AirControlMult = 10, // Air Control? Based on Hover Mode, command 859100 is loading Hover Mode Air Control stat right before 859099 which modifies this stat
-
-    Unknown_4 = 4, // Damage Resistance? Boomerang, Heavy Armor
-    Unknown_56 = 56, // Jet Energy Consumption? Damage Resistance? Heavy Armor
+    TurnSpeed = 27,
 
     // TODO: Map out these
-    Unknown_2 = 2,
-    Unknown_3 = 3,
-    Unknown_5 = 5,
-    Unknown_6 = 6,
-    Unknown_7 = 7,
-    Unknown_8 = 8,
-    Unknown_9 = 9,
-    Unknown_11 = 11,
-    Unknown_12 = 12,
-    Unknown_13 = 13,
-    Unknown_14 = 14,
-    Unknown_15 = 15,
-    Unknown_17 = 17,
-    Unknown_18 = 18,
-    Unknown_19 = 19,
-    Unknown_23 = 23,
-    Unknown_24 = 24,
-    Unknown_25 = 25,
     Unknown_26 = 26,
     Unknown_28 = 28,
     Unknown_29 = 29,
@@ -48,15 +43,12 @@ public enum StatModifierIdentifier : ushort
     Unknown_38 = 38,
     Unknown_39 = 39,
     Unknown_47 = 47,
+    
+    GravityMult = 50,
+    Unknown_56 = 56, // Jet Energy Consumption? Damage Resistance? Heavy Armor // AirResMult?
     Unknown_57 = 57,
-    FwdRunSpeedMult = 9901,
-    JumpHeightMult = 9902,
-    ThrustStrengthMult = 9904,
-    ThrustAirControl = 9905,
-    AirResistanceMult = 9913,
-    Friction = 9906,
-    TimeDilation = 9910,
-    AccuracyModifier = 9911,
     WeaponChargeupMod = 9914,
     WeaponDamageDealtMod = 9915,
+    AirResistanceMult = 9913,
+    TimeDilation = 9910,
 }
