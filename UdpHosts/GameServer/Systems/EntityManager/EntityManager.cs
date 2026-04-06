822954b49386b4f1a0095c6214fb06556b62ec9c
Sleepwalker <webmaster@teambd.net>
2026-04-06 07:43:56 +0200

    feat: add support for flushing view changes for OutpostEntity in EntityManager

--- UdpHosts/GameServer/Systems/EntityManager/EntityManager.cs
+++ UdpHosts/GameServer/Systems/EntityManager/EntityManager.cs
@@ -1766,6 +1766,10 @@ public class EntityManager
             FlushViewChangesToScoped(vehicle.Vehicle_CombatView, vehicle.EntityId);
             FlushViewChangesToScoped(vehicle.Vehicle_MovementView, vehicle.EntityId);
         }
+        else if (entity is OutpostEntity outpost)
+        {
+            FlushViewChangesToScoped(outpost.Outpost_ObserverView, outpost.EntityId);
+        }
         else if (entity is DeployableEntity deployable)
         {
             FlushViewChangesToScoped(deployable.Deployable_ObserverView, deployable.EntityId);
