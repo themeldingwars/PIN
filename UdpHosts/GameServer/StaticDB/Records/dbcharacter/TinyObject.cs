bd63748cc705584c156fb3eaf616cefed765ee29
Xsear <Xsear@users.noreply.github.com>
2026-04-06 13:57:34 +0200

    Add another 12 SDB tables to SDBInterface
    FactionRelations
    FactionReputations
    PoseType
    CharInfo
    DamageType
    DamageResponse
    DamageResponseDamageType
    TinyObject
    PhysicsMaterial
    VisualRecord
    BattleframeVisuals
    HullSegmentDef

--- UdpHosts/GameServer/StaticDB/Records/dbcharacter/TinyObject.cs
+++ UdpHosts/GameServer/StaticDB/Records/dbcharacter/TinyObject.cs
@@ -2,11 +2,11 @@ namespace GameServer.Data.SDB.Records.dbcharacter;
 public record class TinyObject
 {
     public float Size { get; set; }
-    public int SpawnPfxId { get; set; }
-    public int PosefileId { get; set; }
-    public int PfxId { get; set; }
-    public int SpawnStatusfxId { get; set; }
-    public int Id { get; set; }
-    public int HitStatusfxId { get; set; }
-    public int Flags { get; set; }
+    public uint SpawnPfxId { get; set; }
+    public uint PosefileId { get; set; }
+    public uint PfxId { get; set; }
+    public uint SpawnStatusfxId { get; set; }
+    public uint Id { get; set; }
+    public uint HitStatusfxId { get; set; }
+    public uint Flags { get; set; }
 }
